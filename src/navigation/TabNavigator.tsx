88737a31451660ec851e07a4db84e5888ce30fd9
Liza Tinku Jose <ltinkujose@ucdavis.edu>
2025-04-19 15:56:23 -0700

    basic events listin with filters

---
+++ src/navigation/TabNavigator.tsx
@@ -0,0 +1,19 @@
+// src/navigation/TabNavigator.tsx
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MapScreen from '../screens/MapScreen';
+import EventsScreen from '../screens/EventsScreen'; 
+import { Text } from 'react-native';
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={{ headerShown: true, tabBarLabelPosition: 'beside-icon' }}
+    >
+      <Tab.Screen name="Map" component={MapScreen} />
+      <Tab.Screen name="Events" component={EventsScreen} />
+    </Tab.Navigator>
+  );
+}
